045e168f3f0b26dc7064a799193165fe7a90c87d
Colin Fay <colin@thinkr.fr>
2018-06-07 14:37:35 +0200

    added a space

--- R/browse.R
+++ R/browse.R
@@ -6,6 +6,6 @@ unbrowse <- function(){
 
 browse <- function(){
   a <- rstudioapi::getSourceEditorContext()
-  a$selection[[1]]$text <- gsub('#browser\\(\\)', 'browser()', a$selection[[1]]$text)
+  a$selection[[1]]$text <- gsub('#browser\\(\\)', ' browser()', a$selection[[1]]$text)
   rstudioapi::insertText(location = a$selection[[1]]$range, a$selection[[1]]$text)
 }
